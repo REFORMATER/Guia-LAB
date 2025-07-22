3f0fa4224d2bb43b093924b4a96abbecbdfef560
REFORMATER <ickk1771@gmail.com>
2025-07-21 19:05:29 -0500

    Add files via upload

---
+++ Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    public static void main(String [] args){
+        System.out.println("Mi nombre es Denny Joaquin Mamani Huayhua\nTengo 18 años y vivo en Arequipa"
+        +"\nMe gusta los videojuegos y escuchar musica en mis tiempos libre"+
+        "\nSuelo ser alguien frio y ácido pero tambien puedo ser algo payaso por la situación");
+    }
+}
